3634793809e249250201cf0ea00a4756f4639212
mdellaiera <63198190+mdellaiera@users.noreply.github.com>
2020-12-12 16:09:43 +0100

    Delete Hamming.m

--- KalmanAutoRegressiveModels/Sources/TimeSegmentation/Hamming.m
+++
@@ -1,8 +0,0 @@
-function hamming = Hamming(number_of_points)
-
-alpha = 25/46;
-x = -floor(number_of_points / 2) : floor((number_of_points - 1) / 2);
-hamming = alpha + (1 - alpha) * cos(2 * pi * x / number_of_points)';
-
-end
-
